772e71d031a480d73f9f8c9f1e098cc8b8656d84
Igor8291 <163037219+Igor8291@users.noreply.github.com>
2024-05-26 13:51:29 +0500

    Add files via upload

---
+++ task/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите строки через запятую:");
+string userInput = Console.ReadLine();
+string[] userArray = userInput.Split(',');
+Console.WriteLine(string.Join(", ", FilterShortStrings(userArray)));
+
+static string[] FilterShortStrings(string[] inputArray)
+{
+  // Считаем количество строк длиной <= 3
+  int count = 0;
+  foreach (string str in inputArray)
+  {
+    if (str.Length <= 3)
+    {
+      count++;
+    }
+  }
+
+  string[] resultArray = new string[count];
+  int index = 0;
+  foreach (string str in inputArray)
+  {
+    if (str.Length <= 3)
+    {
+      resultArray[index] = str;
+      index++;
+    }
+  }
+
+  return resultArray;
+}
